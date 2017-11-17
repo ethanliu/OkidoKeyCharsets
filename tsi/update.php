273143632d3bf4184e668f73d52294faaec83ff2
Ethan Liu <ethan@creativecrap.com>
2017-11-17 19:45:00 +0800

    Remove from repo

--- tsi/update.php
+++
@@ -1,7 +0,0 @@
-<?php
-$url = "https://github.com/openvanilla/McBopomofo/raw/master/Source/Data/BPMFMappings.txt";
-$contents = file_get_contents($url);
-$contents = strip_tags($contents);
-// var_dump($contents);
-file_put_contents("./tsi.txt", $contents);
-
