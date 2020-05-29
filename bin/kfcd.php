676793fc04c747c75661d92aba040cdd5cdd8889
Ethan Liu <ethan@creativecrap.com>
2020-05-29 17:59:29 +0800

    words.hk

--- bin/kfcd.php
+++ bin/kfcd.php
@@ -1,10 +1,17 @@
 <?php
 /**
  * kfcd parser
+ * Source:
+ * http://www.kaifangcidian.com/xiazai/cidian_zhyue-kfcd.zip
+ * https://words.hk/faiman/analysis/existingwordcount.json
  *
  * @author Ethan Liu
  * @copyright Creativecrap.com, 27 May, 2020
  */
+
+
+$src = __DIR__ . "/../tmp/existingwordcount.json";
+$weights = json_decode(file_get_contents($src), true);
 
 $src = __DIR__ . "/../tmp/cidian_zhyue-ft-kfcd-yp-2019623.txt";
 $raw = file_get_contents($src);
@@ -13,6 +20,7 @@ $raw = json_decode(mb_substr($raw, $offset), true);
 
 $phrase = "";
 $pinyin = "";
+$weight = 0;
 $matches = [];
 
 foreach ($raw as $index => $item) {
@@ -21,7 +29,8 @@ foreach ($raw as $index => $item) {
 	if (empty($matches)) {
 		if (!empty($phrase) && !empty($pinyin)) {
 			if (mb_strlen($phrase) > 1) {
-				echo "{$phrase}\t0\t{$pinyin}\n";
+				$weight = (isset($weights[$phrase])) ? intval($weights[$phrase]) * 1000 : 0;
+				echo "{$phrase}\t{$weight}\t{$pinyin}\n";
 			}
 		}
 		$phrase = $item;
