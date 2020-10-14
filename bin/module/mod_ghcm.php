88b8fcf544bc2ed2467200c7ff10c988112ab812
Ethan Liu <ethan@creativecrap.com>
2020-10-14 19:03:56 +0800

    "#" as comments issue fixes

--- bin/module/mod_ghcm.php
+++ bin/module/mod_ghcm.php
@@ -9,6 +9,9 @@
  * @package module
  */
 
+// TODO: update description from readme
+// TODO: update half_shape from SM.schema.yaml
+
 
 $raw = explode("\n", file_get_contents($srcPath));
 
@@ -42,7 +45,11 @@ function parse($raw, $mode = '', bool $partial, bool $prepend) {
 			continue;
 		}
 
-		$line = trim(preg_replace('/#(.?)*/', '', $line));
+		// keep #
+		// echo "before: " . $line . "\n";
+		// $line = trim(preg_replace('/#(.?)*/', '', $line));
+		// echo "after: " . $line . "\n";
+
 		if (empty($line)) {
 			continue;
 		}
@@ -50,6 +57,11 @@ function parse($raw, $mode = '', bool $partial, bool $prepend) {
 		$row = explode("\t", $line);
 		if (empty($row)) {
 			continue;
+		}
+
+		if (count($row) < 2) {
+			var_dump($row);
+			die($line);
 		}
 
 		$phrase = trim($row[0]);
@@ -80,52 +92,63 @@ function parse($raw, $mode = '', bool $partial, bool $prepend) {
 	if ($prepend) {
 echo "#
 # 矧码 (ghcm)
+# https://github.com/zeylei/ghcm
 #
 # 方案名称：矧码
 # 方案作者：矧可射思
-# 方案版本：2020-10-11
+# 方案版本：2020-10-14
 #
 # 本仓库中的矧码码表与官方 QQ 群 (461618919) 同步更新。
 # 由作者矧可射思授权，任何人皆可以修改、复制、转发，以及免费内建此词库（包括收费使用）。
 # 学习矧码请加 QQ 群 (461618919)。
+#
+# ==========
+#
+# 此 cin 表格由 SM.dict.yaml 转换
+# 由于部份符号为 rime 另行定义，因此未能直接于 cin 表格中实现，此档仅包含「,」键符号。
+# 而「i」键原定义为直出「，」而非字根，但为统一输入行为，仍先将「i」键列为字根。
+#
 #
 %ename ghcm
 %cname 矧码
 %encoding UTF-8
 %selkey 1ex\\234890
 %keyname begin
-a a
-b b
-c c
-d d
-e e
-f f
-g g
-h h
-i i
-j j
-k k
-l l
-m m
-n n
-o o
-p p
-q q
-r r
-s s
-t t
-u u
-v v
-w w
-x x
-y y
-z z
-. .
-, ,
-; ;
-/ /
+a	a
+b	b
+c	c
+d	d
+e	e
+f	f
+g	g
+h	h
+i	i
+j	j
+k	k
+l	l
+m	m
+n	n
+o	o
+p	p
+q	q
+r	r
+s	s
+t	t
+u	u
+v	v
+w	w
+x	x
+y	y
+z	z
+.	.
+,	,
+;	;
+/	/
 %keyname end
 %chardef begin
+,	、
+,	；
+,	#
 ";
 	}
 
