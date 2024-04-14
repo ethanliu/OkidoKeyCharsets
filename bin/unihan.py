1403da880a33164785df0da0d998e052ab75db1a
Ethan Liu <ethan@creativecrap.com>
2024-04-14 19:02:20 +0800

    Unihan

--- bin/unihan.py
+++ bin/unihan.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 #
+# version: 0.1.0
 # autor: Ethan Liu
 #
 # Unihan.db generator
@@ -8,6 +9,7 @@
 
 import argparse
 import importlib
+import json
 import sys, os
 # import re
 import csv
@@ -15,126 +17,13 @@ import csv
 import sqlite3
 import xml.etree.ElementTree as xet
 from tqdm import tqdm
-from enum import IntEnum
+# from enum import IntEnum
 
 uu = importlib.import_module("lib.util")
+from lib.unihan import Classified, UnihanChar
 
 basedir = os.path.normpath(os.path.join(os.path.dirname(__file__), os.path.pardir))
 
-# https://www.unicode.org/reports/tr38/#kCompatibilityVariant
-
-# or ignore range? F900–FAFF，2F800–2FA1F
-class Classified(IntEnum):
-    Common = 1
-    LessCommon = 2
-    Rare = 3
-    Newly = 10
-    # for sorting friendly purpose, giving a large number instead of zero
-    Unclassified = 99
-
-    @staticmethod
-    def fromCategory(category):
-        match category:
-            case 'A':
-                return Classified.Common
-            case 'B':
-                return Classified.LessCommon
-            case 'C':
-                return Classified.Rare
-            case 'N':
-                return Classified.Newly
-            case _:
-                return Classified.Unclassified
-
-
-class Char:
-    def __init__(self, node):
-        self.cp = node.get('cp')
-        self.text = chr(int(self.cp, 16))
-        # kAlternateTotalStrokes
-        # kFrequency: [1-5]
-        # A rough frequency measurement for the character based on analysis of traditional Chinese USENET postings; characters with a kFrequency of 1 are the most common, those with a kFrequency of 2 are less common, and so on, through a kFrequency of 5.
-        self.frequency = int(node.get('kFrequency') or '0')
-        # kGradeLevel: [1-6], 1: easy, 6: hard
-        # The primary grade in the Hong Kong school system by which a student is expected to know the character; this data is derived from 朗文初級中文詞典, Hong Kong: Longman, 2001.
-        self.gradeLevel = int(node.get('kGradeLevel') or '0')
-        # self.pinyin = node.get('kHanyuPinyin')
-        # kMandarin: [a-z\x{300}-\x{302}\x{304}\x{308}\x{30C}]+
-        self.mandarin = node.get('kMandarin')
-        if self.mandarin:
-            self.mandarin = uu.stripAccents(self.mandarin)
-        # kPhonetic: [1-9][0-9]{0,3}[A-Dx]?[*+]?
-        # self.phonetic = node.get('kPhonetic')
-        # kSpecializedSemanticVariant
-        # kSpoofingVariant
-        # kStrange
-        self.strange = node.get('kStrange')
-        # self.fenn = node.get('kFenn')
-        self.tsource = node.get('kIRG_TSource')
-        # kTotalStrokes
-        # The total number of strokes in the character (including the radical). When there are two values, then the first is preferred for zh-Hans (CN) and the second is preferred for zh-Hant (TW). When there is only one value, it is appropriate for both.
-        self.stroke = 0
-        if node.get('kTotalStrokes'):
-            strokes = node.get('kTotalStrokes').split()
-            # preferred for zh-Hant (TW)
-            self.stroke = int(strokes[-1])
-
-        # kSimplifiedVariant: U\+[23]?[0-9A-F]{4}
-        # self.simplifiedVariant = node.get('kSimplifiedVariant')
-        # if self.simplifiedVariant:
-        #     self.simplifiedVariant = chr(int(self.simplifiedVariant[2:], 16))
-
-        self.simplifiedVariant = None
-        if node.get('kSimplifiedVariant'):
-            self.simplifiedVariant = []
-            items = node.get('kSimplifiedVariant').split()
-            for item in items:
-                c = chr(int(item[2:], 16))
-                if c == self.text:
-                    continue
-                self.simplifiedVariant.append(c)
-
-        # kTraditionalVariant
-        self.traditionalVariant = None
-        if node.get('kTraditionalVariant'):
-            self.traditionalVariant = []
-            items = node.get('kTraditionalVariant').split()
-            for item in items:
-                c = chr(int(item[2:], 16))
-                if c == self.text:
-                    continue
-                self.traditionalVariant.append(c)
-            self.traditionalVariant.reverse()
-        # self.traditionalVariant = chr(int(self.traditionalVariant[2:], 16))
-
-        # score
-        score = 0
-        if self.gradeLevel:
-            score += 7 - self.gradeLevel
-        if self.frequency:
-            score += 6 - self.frequency
-        if self.mandarin:
-            score += 1
-        if not self.strange:
-            score += 1
-        if not self.traditionalVariant:
-            score += 1
-        self.score = score
-
-        self.classified = Classified.Unclassified
-        # if self.traditionalVariant:
-        #     self.classified = Classified.Simplified
-        # if self.strange:
-        #     self.classified = 1
-        # elif self.traditionalVariant:
-        #     if len(self.traditionalVariant) >= 2:
-        #         self.classified = 2
-        #     else:
-        #         self.classified = 3
-
-    def __str__(self):
-        # \tStroke: {self.stroke}\tFreq: {self.frequency}\tLevel: {self.gradeLevel}
-        return f"{self.cp} => {self.text}\tTS: {self.tsource}\tStrange: {self.strange}\tScore: {self.score}\tMandarin: {self.mandarin}\tHans: {self.simplifiedVariant}\tHant: {self.traditionalVariant}"
 
 def importWeight(cursor):
     path = f"{basedir}/rawdata/McBopomofo/Source/Data/phrase.occ"
@@ -205,8 +94,8 @@ def importClassified(cursor):
         fp.close()
     cursor.execute("COMMIT TRANSACTION")
 
-def parseRadical(cursor, repo, namespace):
-    query = "INSERT INTO `radical` (`radical`, `pinyin`, `classified`, `score`, `stroke`, `weight`) VALUES (:radical, :pinyin, :classified, :score, :stroke, :weight)"
+def importUnihanRadical(cursor, repo, namespace):
+    query = "INSERT INTO `radical` (`radical`, `pinyin`, `definition`, `classified`, `score`, `stroke`, `weight`) VALUES (:radical, :pinyin, :definition, :classified, :score, :stroke, :weight)"
 
     cursor.execute("BEGIN TRANSACTION")
 
@@ -214,11 +103,12 @@ def parseRadical(cursor, repo, namespace):
         for child in tqdm(chunk, desc = f"Unihan[Radical]", unit = 'MB', unit_scale = True, ascii = True):
         # for child in tqdm(repo.findall('ucd:char', namespace), unit = 'MB', unit_scale = True, ascii = True, desc = f"Radical"):
         # for child in repo.findall('ucd:char', namespace):
-            char = Char(child)
+            char = UnihanChar(child)
 
             params = {
                 "radical": char.text,
                 "pinyin": char.mandarin or None,
+                "definition": char.definition or None,
                 "classified": char.classified,
                 "score": char.score,
                 "stroke": char.stroke,
@@ -226,72 +116,69 @@ def parseRadical(cursor, repo, namespace):
             }
             cursor.execute(query, params)
 
-        cursor.execute("COMMIT TRANSACTION")
+    cursor.execute("COMMIT TRANSACTION")
 
-def parseSimplified(cursor, repo, namespace):
+def importUnihanLocale(cursor, repo, namespace, table):
     query1 = "SELECT `rowid` FROM `radical` WHERE `radical` = :radical LIMIT 1"
-    query2 = "INSERT INTO `t2s` (`hant_id`, `hans_id`) VALUES (:hant_id, :hans_id)"
+    query2 = f"INSERT INTO `{table}` (`hant_id`, `hans_id`) VALUES (:hant_id, :hans_id)"
+
+    if table == "t2s":
+        target = "simplifiedVariant"
+        radical_name = "hant_id"
+        target_name = "hans_id"
+    else:
+        target = "traditionalVariant"
+        radical_name = "hans_id"
+        target_name = "hant_id"
 
     cursor.execute("BEGIN TRANSACTION")
-
     for chunk in uu.chunks(repo.findall('ucd:char', namespace), max = 0):
-        for child in tqdm(chunk, desc = f"Unihan[SC]", unit = 'MB', unit_scale = True, ascii = True):
-        # for child in tqdm(repo.findall('ucd:char', namespace), unit = 'MB', unit_scale = True, ascii = True, desc = f"t2s"):
-        # for child in repo.findall('ucd:char', namespace):
-            char = Char(child)
-            if not char.simplifiedVariant:
+        for child in tqdm(chunk, desc = f"Unihan[{table}]", unit = 'MB', unit_scale = True, ascii = True):
+            char = UnihanChar(child)
+            variants = getattr(char, target)
+            if not variants:
                 continue
 
-            hant_id = uu.getOne(cursor, query1, {"radical": char.text})
-            if not hant_id:
-                print("Radical not found")
-                print(char)
-                sys.exit()
+            radical_id = uu.getOne(cursor, query1, {"radical": char.text})
+            if not radical_id:
+                tqdm.write(f"Radical Not found: {char.text}")
+                continue
 
-            for c in char.simplifiedVariant:
-                hans_id = uu.getOne(cursor, query1, {"radical": c})
-                if not hans_id:
-                    print("Hans radical not found")
-                    print(char)
-                    # sys.exit()
+            for variant in variants:
+                target_id = uu.getOne(cursor, query1, {"radical": variant})
+                if not target_id:
+                    tqdm.write(f"Radical Not found: {variant}")
                     continue
-                cursor.execute(query2, {"hant_id": hant_id, "hans_id": hans_id})
-                # tqdm.write(f"[t2s]: {char.text} => {c}")
-
+                cursor.execute(query2, {radical_name: radical_id, target_name: target_id})
     cursor.execute("COMMIT TRANSACTION")
 
-def parseTraditional(cursor, repo, namespace):
-    query1 = "SELECT `rowid` FROM `radical` WHERE `radical` = :radical LIMIT 1"
-    query2 = "INSERT INTO `s2t` (`hans_id`, `hant_id`) VALUES (:hans_id, :hant_id)"
+def importTongwenLocale(cursor, table, dir):
+    def _import(path):
+        query1 = "SELECT `rowid` FROM `radical` WHERE `radical` = :radical LIMIT 1"
+        query2 = f"INSERT INTO `{table}` (`hans_id`, `hant_id`) VALUES (:hans_id, :hant_id)"
+        prefix = f"Tongwen[{table}]"
+        if table == "t2s":
+            radical_name = "hant_id"
+            target_name = "hans_id"
+        else:
+            radical_name = "hans_id"
+            target_name = "hant_id"
+        with open(path, 'r') as file:
+            data = json.load(file)
+            cursor.execute("BEGIN TRANSACTION")
+            for key, value in tqdm(data.items(), desc = prefix, unit = 'MB', unit_scale = True, ascii = True):
+                # print(f"{key} => {value}")
+                radical_id = uu.getOne(cursor, query1, {"radical": key})
+                target_id = uu.getOne(cursor, query1, {"radical": value})
+                if not radical_id or not target_id:
+                    tqdm.write(f"{prefix} Radical not found: {key} => {value}")
+                    continue
+                cursor.execute(query2, {radical_name: radical_id, target_name: target_id})
+            cursor.execute("COMMIT TRANSACTION")
+    _import(f"{dir}/{table}-char.json")
+    # _import(f"{dir}/s2t-phrase.json")
 
-    cursor.execute("BEGIN TRANSACTION")
-
-    for chunk in uu.chunks(repo.findall('ucd:char', namespace), max = 0):
-        for child in tqdm(chunk, desc = f"Unihan[TC]", unit = 'MB', unit_scale = True, ascii = True):
-        # for child in tqdm(repo.findall('ucd:char', namespace), unit = 'MB', unit_scale = True, ascii = True, desc = f"s2t"):
-        # for child in repo.findall('ucd:char', namespace):
-            char = Char(child)
-
-            if not char.traditionalVariant:
-                continue
-
-            hans_id = uu.getOne(cursor, query1, {"radical": char.text})
-            if not hans_id:
-                print("Radical not found")
-                print(char)
-                sys.exit()
-
-            for c in char.traditionalVariant:
-                hant_id = uu.getOne(cursor, query1, {"radical": c})
-                if not hant_id:
-                    print("Hant radical not found")
-                    print(char)
-                    sys.exit()
-                cursor.execute(query2, {"hant_id": hant_id, "hans_id": hans_id})
-
-    cursor.execute("COMMIT TRANSACTION")
-
-def importRadical(cursor, task):
+def importUnihan(cursor, task):
     path = f"{basedir}/rawdata/Unihan/ucd.unihan.flat.xml"
     namespace = {
         'ucd': 'http://www.unicode.org/ns/2003/ucd/1.0',
@@ -300,38 +187,20 @@ def importRadical(cursor, task):
     repo = root.find('ucd:repertoire', namespace)
 
     if task == "radical":
-        parseRadical(cursor, repo, namespace)
-    elif task == "t2s":
-        parseSimplified(cursor, repo, namespace)
-    elif task == "s2t":
-        parseTraditional(cursor, repo, namespace)
-
-# def test(cursor):
-#     query = '''SELECT
-#         t.radical AS hant,
-#         s.radical AS hans
-#     FROM t2s
-#         LEFT JOIN unihan AS t ON (t.rowid = t2s.hant_id)
-#         LEFT JOIN unihan AS s ON (s.rowid = t2s.hans_id)'''
-#     rows = cursor.execute(query)
-#     print(rows)
+        importUnihanRadical(cursor, repo, namespace)
+    else:
+        importUnihanLocale(cursor, repo, namespace, task)
 
 
-def main():
-    argParser = argparse.ArgumentParser(description='Unihan Utility')
-    # argParser.add_argument('-i', '--input', help='base dir of the repo')
-    argParser.add_argument('-o', '--output', default='', help='output db path')
+def importTongwen(cursor, task):
+    dir = f"{basedir}/rawdata/tongwen-core/dictionaries/"
+    importTongwenLocale(cursor, task, dir)
 
-    args = argParser.parse_args()
-    # performImport(args)
-    # performViewer()
+def createDatabase(path):
+    if os.path.isfile(path):
+        os.remove(path)
 
-    if os.path.isfile(args.output):
-        os.remove(args.output)
-
-    # create db
-
-    db = sqlite3.connect(args.output)
+    db = sqlite3.connect(path)
     cursor = db.cursor()
 
     # the radical based on unihan
@@ -339,6 +208,7 @@ def main():
     cursor.execute('''CREATE TABLE `radical` (
         `radical` CHAR(4) UNIQUE NOT NULL,
         `pinyin` CHAR(10) default NULL,
+        `definition` TEXT default NULL,
         `classified` INTEGER default 0,
         `stroke` INTEGER default 0,
         `score` INTEGER default 0,
@@ -355,13 +225,86 @@ def main():
         `hant_id` INTEGER default 0
     )''')
 
+    db.commit()
+    db.close()
+
+def test(cursor):
+    print("test [t2s]")
+    query = f"SELECT a.hant_id, a.hans_id, t.radical AS hant, s.radical AS hans FROM t2s AS a LEFT JOIN radical AS t ON (a.hant_id = t.rowid) LEFT JOIN radical AS s ON (a.hans_id = s.rowid) ORDER BY random() LIMIT 5"
+    result = uu.getAll(cursor, query)
+    for row in result:
+        print(f"{row.get('hant')} => {row.get('hans')}")
+
+    print("test [s2t]")
+    query = f"SELECT a.hant_id, a.hans_id, t.radical AS hant, s.radical AS hans FROM s2t AS a LEFT JOIN radical AS t ON (a.hant_id = t.rowid) LEFT JOIN radical AS s ON (a.hans_id = s.rowid) ORDER BY random() LIMIT 5"
+    result = uu.getAll(cursor, query)
+    for row in result:
+        print(f"{row.get('hans')} => {row.get('hant')}")
+
+
+    list = '"麵", "面", "后", "後", "體", "園", "國", "国", "体", "园", "台", "臺", "颱"'
+
+    print("test [t2s]")
+    query = f'''SELECT
+        t.radical AS hant,
+        s.radical AS hans
+    FROM t2s AS a
+    LEFT JOIN radical AS t ON (t.rowid = a.hant_id)
+    LEFT JOIN radical AS s ON (s.rowid = a.hans_id)
+    WHERE hant IN ({list})
+    ORDER BY hant
+    '''
+    result = uu.getAll(cursor, query)
+    for row in result:
+        print(f"{row.get('hant')} => {row.get('hans')}")
+
+    print("test [s2t]")
+    query = f'''SELECT
+        t.radical AS hant,
+        s.radical AS hans
+    FROM s2t AS a
+    LEFT JOIN radical AS t ON (t.rowid = a.hant_id)
+    LEFT JOIN radical AS s ON (s.rowid = a.hans_id)
+    WHERE hans IN ({list})
+    ORDER BY hans
+    '''
+    result = uu.getAll(cursor, query)
+    for row in result:
+        print(f"{row.get('hans')} => {row.get('hant')}")
+
+
+def main():
+    argParser = argparse.ArgumentParser(description='Unihan Utility')
+    # argParser.add_argument('-i', '--input', help='base dir of the repo')
+    argParser.add_argument('-o', '--output', default='', help='output db path')
+    args = argParser.parse_args()
+
+    preferTonwen = False
+    dryrun = True
+
+    if not dryrun:
+        createDatabase(args.output)
+
+    db = sqlite3.connect(args.output)
+    db.row_factory = uu.dict_factory
+    # db.row_factory = sqlite3.Row
+    cursor = db.cursor()
+
     cursor.execute("PRAGMA synchronous = OFF")
     cursor.execute("PRAGMA journal_mode = MEMORY")
 
-    importRadical(cursor, "radical")
-    importRadical(cursor, "t2s")
-    importRadical(cursor, "s2t")
-    # importClassified(cursor)
+    if dryrun:
+        test(cursor)
+        sys.exit(0)
+
+    importUnihan(cursor, "radical")
+    if preferTonwen:
+        importTongwen(cursor, "t2s")
+        importTongwen(cursor, "s2t")
+    else:
+        importUnihan(cursor, "t2s")
+        importUnihan(cursor, "s2t")
+    # ## importClassified(cursor)
     importWeight(cursor)
     # test(cursor)
 
