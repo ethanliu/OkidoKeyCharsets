d1628436b2534e4f1f914b62228f8a47a6575fd1
Ethan Liu <ethan@creativecrap.com>
2024-08-23 03:01:47 +0800

    MoE data process
    the resources from MoE are not well formatted and inconsistently
    therefore, distract phrase only and filter out short phrase and undefined characters

--- bin/moe2csv.py
+++ bin/moe2csv.py
@@ -11,121 +11,40 @@ import argparse
 import sys, os
 import csv
 import re
-import codecs
+# import codecs
+import pinyin as tp
 from tqdm import tqdm
 from lib.util import chunks
 
-kAccentsMap = {
-    u'ā': u'a', u'ɑ̄': u'a', u'ē': u'e', u'ī': u'i', u'ō': u'o', u'ū': u'u', u'ǖ': u'u',
-    u'Ā': u'A', u'Ē': u'E', u'Ī': u'I', u'Ō': u'O', u'Ū': u'U', u'Ǖ': u'U',
-    u'á': u'a', u'ɑ́': u'a', u'é': u'e', u'í': u'i', u'ó': u'o', u'ú': u'u', u'ǘ': u'u',
-    u'Á': u'A', u'É': u'E', u'Í': u'I', u'Ó': u'O', u'Ú': u'U', u'Ǘ': u'U',
-    u'ǎ': u'a', u'ɑ̌': u'a', u'ě': u'e', u'ǐ': u'i', u'ǒ': u'o', u'ǔ': u'u', u'ǚ': u'u',
-    u'Ǎ': u'A', u'Ě': u'E', u'Ǐ': u'I', u'Ǒ': u'O', u'Ǔ': u'U', u'Ǚ': u'U',
-    u'à': u'a', u'ɑ̀': u'a', u'è': u'e', u'ì': u'i', u'ò': u'o', u'ù': u'u', u'ǜ': u'u',
-    u'À': u'A', u'È': u'E', u'Ì': u'I', u'Ò': u'O', u'Ù': u'U', u'Ǜ': u'U',
-    u'a': u'a', u'ɑ': u'a', u'e': u'e', u'i': u'i', u'o': u'o', u'u': u'u', u'ü': u'u',
-    u'A': u'A', u'E': u'E', u'I': u'I', u'O': u'O', u'U': u'U', u'Ü': u'U',
-}
-
-kStripMpsSpaceSeparate = True
-kMinPhraseLength = 2
-kMps1Set = set(' ˇˊˋ˙ㄅㄆㄇㄈㄉㄊㄋㄌㄍㄎㄏㄐㄑㄒㄓㄔㄕㄖㄗㄘㄙㄚㄛㄜㄝㄞㄟㄠㄡㄢㄣㄤㄥㄦㄧㄨㄩ')
-kPattern1 = r"[（\(][變|一|二|三|四|五|六|語音|讀音|又音|\d]+[）\)]|<br>"
-
-def asciify(error):
-    return kAccentsMap[error.object[error.start]], error.end
-
-def trimMps1(str):
-    return ''.join(c for c in str if c in kMps1Set)
-
-def trimMps2(str):
-    str = str.strip()
-    try:
-        str = str.encode('ascii', 'asciify').decode()
-    except:
-        pass
-    return str
-
-def indexExists(list, index):
-    try:
-        list[index]
-        return True
-    except IndexError:
-        return False
-
-def uniqueList(rows):
-    return list(dict.fromkeys(filter(None, rows)))
-
-def parse(inputPath, outputPath):
-
+def parse2(inputPath, outputPath):
     filename = os.path.basename(inputPath)
-    column = []
+    # [A-Z][a-z][0-9] etc?
+    pattern = f"[，。「」“”『』）【】\"\',.]+"
+    skipHeader = True
     contents = ""
 
     with open(inputPath) as fp:
         reader = csv.reader(fp, delimiter = ',')
         for chunk in chunks(reader, max = 0):
             for row in tqdm(chunk, desc = f"{filename}[]", unit = 'MB', unit_scale = True, ascii = True):
-                if not column:
-                    column = row
+                if not row[0]:
                     continue
 
-                # code = row[0].strip()
-                phrase = row[1].strip()
-
-                if not phrase or len(phrase) < kMinPhraseLength:
-                    # print(f"Ignore short phrase: {phrase}")
+                if skipHeader:
+                    skipHeader = False
                     continue
 
-                # if ".gif" in phrase or ".png" in phrase:
-                #     # TODO: unicode mapping
-                #     # print(f"Ignore legacy phrase: {phrase}")
-                #     continue
+                phrase = row[0].strip()
 
-                mps1 = ' '.join([node.strip() for node in row[2].split('\u3000')])
-                mps1 = re.split(kPattern1, trimMps1(mps1))
+                if len(phrase) < 2 or ".gif" in phrase or ".png" in phrase or phrase.startswith("_x"):
+                    continue
 
-                mps2 = ' '.join([node.strip() for node in row[3].split('\u3000')])
-                mps2 = [trimMps2(node) for node in re.split(kPattern1, mps2)]
-
-                if indexExists(row, 4):
-                    extra = row[4].replace('<br>', '\u3000')
-                    extra = ' '.join([node.strip() for node in extra.split('\u3000')])
-                    extra = ' '.join(list(filter(None, [node.strip() for node in re.split(kPattern1, extra)])))
-                    extra = [node.strip() for node in extra.split()]
-
-                    for item in extra:
-                        item = item.strip()
-                        try:
-                            item = item.encode('ascii', 'asciify').decode()
-                            mps2.append(item)
-                        except:
-                            mps1.append(trimMps1(item))
-
-                mps1 = uniqueList(mps1)
-                mps2 = uniqueList(mps2)
-
-                #     print(phrase)
-                #     print(f"mps1: {row[2]} => {mps1}")
-                #     print(f"mps2: {row[3]} => {mps2}")
-
-                for mps in mps2:
-                    # print(f"{phrase}\t0\t{mps}")
-                    if kStripMpsSpaceSeparate:
-                        contents += f"{phrase}\t0\t{''.join(mps.split())}\n"
-                    else:
-                        contents += f"{phrase}\t0\t{mps}\n"
-
-        # fp.close()
-
-    # if not outputPath:
-    #     print(contents)
-    #     return
+                _phrase = re.sub(pattern, "", phrase)
+                pinyin = tp.get(_phrase, format = "strip", delimiter = "")
+                contents += f"{phrase}\t0\t{pinyin}\n"
 
     with open(outputPath, 'w') as fp:
         fp.write(contents)
-        fp.close()
 
 
 def main():
@@ -140,9 +59,8 @@ def main():
     if not os.path.exists(args.input):
         sys.exit(f"File not found: {args.input}")
 
-    codecs.register_error('asciify', asciify)
-
-    parse(args.input, args.output)
+    # codecs.register_error('asciify', asciify)
+    parse2(args.input, args.output)
 
     # if args.category == "concised":
     #     parseConcised(args.path, args.output)
