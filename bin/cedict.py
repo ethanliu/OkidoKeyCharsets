1058b7694a65a9d35b0a1b17405a2301380c3730
Ethan Liu <ethan@creativecrap.com>
2024-11-27 19:43:27 +0800

    Building tools
    added: cedict spider and check version and download
    changed: moe spider, move to lib, with proper download_dir
    changed: all pinyin with space delimiter and 5 tone style
    changed: keep original "pinyin" from jyutping words
    changed: jieba pinyin and relax whitespace for phrases

---
+++ bin/cedict.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+#
+# autor: Ethan Liu
+#
+# cc-cedict helper
+#
+
+import os
+import sys
+import re
+import argparse
+from tqdm import tqdm
+from lib.util import dir, chunks, trim, strip_accents, write_file
+from lib.cedict_spider import run_spider
+
+# BASE_DIR = dir(__file__ + "/../")
+# SOURCE_PATH = f"{BASE_DIR}/rawdata/cedict/cedict_ts.u8"
+
+def parse(path):
+    pattern = r'(.*) (.*) \[(.*)\] \/(.*)\/'
+    rows = [];
+    with open(path) as fp:
+        for chunk in chunks(fp, max = 0):
+            for row in tqdm(chunk, desc = "CEDICT", unit = 'MB', unit_scale = True, ascii = True):
+                row = trim(row, "#")
+                if row == "":
+                    continue
+                items = [trim(x) for x in re.split(pattern, row) if x]
+                hant = whitespace(items[0])
+                hans = whitespace(items[1])
+                # TODO: middle dot ・, hyphen, comma
+                pinyin = items[2]
+                # pinyin2 = strip_accents(pinyin, True)
+                glosses = [trim(x) for x in re.split(r';', items[3]) if x]
+                # print(f"=> {row}")
+                item = {
+                    "hant": hant,
+                    "hans": hans,
+                    "pinyin": pinyin,
+                    # "pinyin2": pinyin2,
+                    "glosses": ";".join(glosses),
+                }
+                rows.append(item)
+    return rows
+
+def export_csv(rows, locale, path):
+    contents = ""
+    for row in rows:
+        contents += f"{row[locale]}\t0\t{row['pinyin']}\n";
+    write_file(path, contents)
+
+def main():
+    arg_reader = argparse.ArgumentParser(description='')
+    arg_reader.add_argument('-i', '--input', help='cedict_ts.u8 path')
+    arg_reader.add_argument('-o', '--output', default='', help='output dir')
+    arg_reader.add_argument('-d', '--download', action=argparse.BooleanOptionalAction, help='download')
+
+    args = arg_reader.parse_args()
+    # print(args, len(sys.argv))
+
+    if args.download:
+        run_spider(args.output)
+    else:
+        rows = parse(args.input)
+        export_csv(rows, "hant", f"{args.output}/cedict-hant.csv")
+        export_csv(rows, "hans", f"{args.output}/cedict-hans.csv")
+
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Interrupt by user")
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
+    # except BaseException as err:
--- bin/mcbpmf2csv.py
+++ bin/mcbpmf2csv.py
@@ -7,24 +7,64 @@
 import argparse
 import sys, os
 import csv
+import re
 from tqdm import tqdm
 from lib.util import trim, chunks
 from lib.pinyin import PinyinQuery
+from lib.util import chunks, list_flatten, list_unique
+from lib.bpmf import bpmf_remove_tones, bpmf_to_pinyin
 
 # def exam(phrase, pinyin):
 #     if phrase in exam_list:
 #         print(f"-> {phrase} {pinyin}")
 
+mapping = {}
+
+def prepareMapping(input_path):
+    global mapping
+    path = f"{input_path}/Data/BPMFMappings.txt"
+
+    _mapping = {}
+    filename = os.path.basename(path)
+    delimiter = " "
+
+    with open(path) as fp:
+        first_line = fp.readline()
+        fp.seek(0)
+
+        if "\t" in first_line:
+            delimiter = "\t"
+
+        reader = csv.reader(fp, delimiter = delimiter)
+        for chunk in chunks(reader, max = 0):
+            for row in tqdm(chunk, desc = f"{filename}[]", unit = 'MB', unit_scale = True, ascii = True):
+                phrase = trim(row[0] or '')
+                bpmf = " ".join(row[1:])
+                if not phrase or not bpmf:
+                    continue
+
+                pinyin = ""
+                try:
+                    pinyin = re.split(" ", trim(bpmf))
+                    pinyin = " ".join([bpmf_to_pinyin(item) for item in pinyin])
+                except Exception as e:
+                    pinyin = ""
+
+                # print(f"{phrase} => {bpmf} {pinyin}")
+                _mapping[phrase] = pinyin
+
+    mapping = _mapping
+
+
 def parse(input_path, output_path):
-
+    global mapping
+    path = f"{input_path}/Data/phrase.occ"
+    filename = os.path.basename(path)
     qm = PinyinQuery()
-
-    filename = os.path.basename(input_path)
     contents = ""
     delimiter = " "
-    # _dir_ = dir(__file__)
-    # pool = multiprocessing.Pool(workers)
-    with open(input_path) as fp:
+
+    with open(path) as fp:
         first_line = fp.readline()
         fp.seek(0)
 
@@ -39,11 +79,10 @@ def parse(input_path, output_path):
                     continue
 
                 weight = (row[1:2] or ('0', ''))[0]
-                # pinyin = translate(phrase)
-                # pinyin = tp.get(phrase, format = "strip", delimiter = "")
-                # phrase_pinyin = "".join(list_flatten(lazy_pinyin(phrase, strict=False, errors='ignore', style=Style.NORMAL)))
-                # phrase_pinyin = "".join(list_flatten(pinyin(phrase, strict=False, errors='ignore', style=Style.NORMAL)))
-                phrase_pinyin = qm.find(phrase)
+                phrase_pinyin = mapping.get(phrase) or None
+
+                if not phrase_pinyin:
+                    phrase_pinyin = qm.find("hant", phrase)
 
                 if phrase_pinyin == phrase or phrase_pinyin == None:
                     phrase_pinyin = ''
@@ -57,9 +96,33 @@ def parse(input_path, output_path):
         fp.write(contents)
         fp.close()
 
+def updateVersion(input_path):
+    # version...
+    path = f"{input_path}/McBopomofo-Info.plist"
+    contents = ''
+    with open(path, 'r') as fp:
+        contents = fp.read()
+
+    pattern = r"CFBundleShortVersionString</key>[\n\s]+<string>(.*)</string>"
+    result = re.findall(pattern, contents, re.MULTILINE)
+    version = result[0] or None
+
+    if version:
+        print(f"Patching version: {version}")
+        basedir = os.path.normpath(os.path.join(os.path.dirname(__file__), os.path.pardir))
+        path = f"{basedir}/lexicon/mcbopomofo.csv.txt"
+
+        contents = ''
+        with open(path, 'r') as file:
+            contents = file.read()
+        contents = re.sub(r'小麥注音輸入法 (.*) 版本', f"小麥注音輸入法 {version} 版本", contents)
+        # print(contents)
+        with open(path, 'w') as file:
+            file.write(contents)
+
 def main():
     arg_reader = argparse.ArgumentParser(description='McBopomofo tookit')
-    arg_reader.add_argument('-i', '--input', help='Original csv file path')
+    arg_reader.add_argument('-i', '--input', help='Source dir')
     arg_reader.add_argument('-o', '--output', default='', help='Lexicon format csv file path')
 
     args = arg_reader.parse_args()
@@ -68,9 +131,9 @@ def main():
     if not os.path.exists(args.input):
         sys.exit(f"File not found: {args.input}")
 
+    prepareMapping(args.input)
     parse(args.input, args.output)
-
-    sys.exit(0)
+    updateVersion(args.input)
 
 if __name__ == "__main__":
     try:
